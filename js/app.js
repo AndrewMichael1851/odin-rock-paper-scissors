ed09e1db823d274eb69d78a5021538934fcbdc71
AndrewMichael1851 <andrewmichael1851@gmail.com>
2022-03-31 16:47:47 -0400

    One small change to logs.

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ let tests;
 let rock;
 let paper;
 let scissors;
-tests = 10000;
+tests = 1000000;
 rock = 0;
 paper = 0;
 scissors = 0;
@@ -39,9 +39,9 @@ function recordComputerPlay() {
             scissors = scissors + 1;
         }
     }
-    console.log(rock);
-    console.log(paper);
-    console.log(scissors);
+    console.log('R: ' + rock);
+    console.log('P: ' + paper);
+    console.log('S: ' + scissors);
 }
 
 // Run any required functions.
